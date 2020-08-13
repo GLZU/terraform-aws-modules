348d5b8aaeba02a30140db2243e46de1c55d56dc
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-13 00:23:44 +0000

    Update mod_variable.tf

--- modules/TFE/mod_variable/mod_variable.tf
+++ modules/TFE/mod_variable/mod_variable.tf
@@ -1,10 +1,11 @@
 locals{
 	params = var.params
 }
-resource "tfe_variable" "test" {
-  key          = local.param.variable.key								# Name of the variable
-  value        = local.param.variable.value								# value of the variable
-  category     = lookup(local.param.variable, "category", "terraform")  # Setting up default to terraform unless specified
+
+resource "tfe_variable" "variable" {
+  key          = local.params.variable.key								# Name of the variable
+  value        = local.params.variable.value								# value of the variable
+  category     = lookup(local.params.variable, "category", "terraform")  # Setting up default to terraform unless specified
   workspace_id = local.params.variable.ws_id							# Workspace to attach to
-  description  = lookup(local.param.variable,"description","Variable")	# Description of Variable 
+  description  = lookup(local.params.variable,"description","Variable")	# Description of Variable 
 }
