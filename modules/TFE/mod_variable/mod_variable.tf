6456b9bf4cfe819563e14fcede1603206adcff03
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-16 02:27:39 +0000

    Update mod_variable.tf

--- modules/TFE/mod_variable/mod_variable.tf
+++ modules/TFE/mod_variable/mod_variable.tf
@@ -7,7 +7,7 @@ resource "tfe_variable" "variable" {
   value        = local.params.variable.value								# value of the variable
   category     = lookup(local.params.variable, "category", "terraform")  # Setting up default to terraform unless specified
   workspace_id = local.params.variable.ws_id							# Workspace to attach to
- # hcl          = lookup(local.params.variable, "hcl", false) 
-  sensative    = lookup(local.params.variable, "sensative", false) 
+  hcl          = lookup(local.params.variable, "hcl", false) 
+  sensitive    = lookup(local.params.variable, "sensitive", false) 
   description  = lookup(local.params.variable,"description","Variable")	# Description of Variable 
 }
